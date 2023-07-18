26c04462858dabedbef0781bc3d1d638646e784e
Victor Baillet <victor.baillet@polytechnique.edu>
2023-07-17 20:13:59 -0400

    cleaning main.py

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from utils.config.config import parse_args, process_config
 from models.parallel_registration.parallel_registration import parallel_registration_irn
 from models.serial_registration.serial_registration import serial_registration_irn
 
-"""
-Rajouter methodes : .save et .load
-"""
-
 
 def main():
     args = parse_args()
@@ -32,17 +28,23 @@ def main():
             dataset_name=dataset_config['DATASET_NAME'])
     
     
+    contrast_1_path, contrast_2_path, model_path = compute_paths(config_dict)
+    
+    net.save_images(contrast=1, path=contrast_1_path)
+    net.save_images(contrast=2, path=contrast_2_path)
+    net.save_model(model_path)
+    
+def compute_paths(config_dict):
     save_folder = f'runs/{config_dict["DATASET"]["DATASET_NAME"]}/'
     im_dir = save_folder + 'images'
     weights_dir = save_folder + 'weights'
     pathlib.Path(weights_dir).mkdir(parents=True, exist_ok=True)
     pathlib.Path(im_dir).mkdir(parents=True, exist_ok=True)
-    path_contrast_1 = os.path.join(im_dir, 'contrast_1.nii.gz')
-    path_contrast_2 = os.path.join(im_dir, 'contrast_2.nii.gz')
+    contrast_1_path = os.path.join(im_dir, 'contrast_1.nii.gz')
+    contrast_2_path = os.path.join(im_dir, 'contrast_2.nii.gz')
+    model_path = os.path.join(weights_dir, 'model.pt')
     
-    net.save_images(contrast=1, path=path_contrast_1)
-    net.save_images(contrast=2, path=path_contrast_2)
-    net.save_model(os.path.join(weights_dir, 'model.pt'))
+    return contrast_1_path, contrast_2_path, model_path
     
 if __name__ == '__main__':
     main()
